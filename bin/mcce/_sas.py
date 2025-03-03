058cb1d3c96742e9acd7589e532ba6d88516d217
newbooks <10424666+newbooks@users.noreply.github.com>
2025-03-03 10:19:39 -0500

    update

---
+++ bin/mcce/_sas.py
@@ -0,0 +1,8 @@
+"""
+This module contains functions for calculating solvent accessible surface area (SAS) of a protein.
+- sas_atom(atom, background): a function that calculates SAS of an atom in context of background.
+- sas_residue(residue, background): a function that calculates SAS of a residue in context of background.
+
+SAS is calculated using van der Waals radii of atoms and a rolling ball algorithm.
+"""
+
