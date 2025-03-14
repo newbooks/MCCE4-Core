552159a9ba74d8eb35f73e145246da185305a013
Junjun Mao <10424666+newbooks@users.noreply.github.com>
2025-03-14 13:05:07 -0400

    closes #71

--- bin/mcce/_place_missing_heavy_atoms.py
+++ bin/mcce/_place_missing_heavy_atoms.py
@@ -48,19 +48,20 @@ def place_missing_heavy_atoms(self):    # self is a MCCE object
                     missing = should_be_connected - actually_connected - {" ?  "} - completed_missing
                     if missing:
                         n_connected = len(actually_connected)
+                        # get the center atom and the first connected atom
+                        resname = atom.resname
+                        c_atom_p = atom.xyz
+                        c_atom_t = templates[resname][atom.atomname]
+
+                        check_atom = atom.connect12[0]
+                        if check_atom.atomname not in templates[resname]:
+                            logging.error(f"   Connected atom \'{check_atom.atomname}\' to \'{atom.atomname}\' not found in template for residue {resname}")
+                            sys.exit(1)
+                        a_atom_p = check_atom.xyz
+                        a_atom_t = templates[resname][check_atom.atomname]
+
                         if n_connected >= 2:  # at least 2 known connected atoms, place the missing atom
-                            resname = atom.resname
-                            c_atom_p = atom.xyz         # center atom coordinates
-                            c_atom_t = templates[resname][atom.atomname]  # center atom of the template coordinates
-
-                            # will get the two connected atoms
-                            check_atom = atom.connect12[0]  # pick one of the connected atoms
-                            if check_atom.atomname not in templates[resname]:
-                                logging.error(f"   Connected atom \'{check_atom.atomname}\' to \'{atom.atomname}\' not found in template for residue {resname}")
-                                sys.exit(1)
-                            a_atom_p = check_atom.xyz
-                            a_atom_t = templates[resname][check_atom.atomname]
-
+                            # will get another connected atom to the center atom
                             check_atom = atom.connect12[1]  # pick the other connected atom
                             if check_atom.atomname not in templates[resname]:
                                 logging.error(f"   Connected atom \'{check_atom.atomname}\' to \'{atom.atomname}\' not found in template for residue {resname}")
@@ -71,10 +72,44 @@ def place_missing_heavy_atoms(self):    # self is a MCCE object
                             # now we have all the coordinates, we can place the missing atom
                             op = geom_3v_onto_3v(c_atom_t, a_atom_t, b_atom_t, c_atom_p, a_atom_p, b_atom_p)
                             for atom_name in list(missing):
-                                new_atom = Atom()
+                                new_atom = atom.clone()
                                 new_atom.atomname = atom_name
                                 new_atom.xyz = op.apply_to_vector(templates[resname][atom_name])  # apply the transformation to the template coordinates
-                                conf.atoms.append(new_atom)
-                                completed_missing.add(atom_name)
+                                new_atom.connect12.append(atom)
+                                atom.connect12.append(new_atom)
+                                # decide if we need to add this atom to conformer
+                                if new_atom.atomname not in [a.atomname for a in conf.atoms]:
+                                    completed_missing.add(new_atom)
+                            
+                        elif n_connected == 1:
+                            # look for secondarily connected atom
+                            found_second = False
+                            for check_atom2 in check_atom.connect12:
+                                if check_atom2 != atom:
+                                    found_second = True
+                                    break
+                            if found_second:
+                                b_atom_p = check_atom2.xyz
+                                b_atom_t = templates[resname][check_atom2.atomname]
+                                op = geom_3v_onto_3v(c_atom_t, a_atom_t, b_atom_t, c_atom_p, a_atom_p, b_atom_p)
+                                for atom_name in list(missing):
+                                    new_atom = atom.clone()
+                                    new_atom.atomname = atom_name
+                                    new_atom.xyz = op.apply_to_vector(templates[resname][atom_name])
+                                    new_atom.connect12.append(atom)
+                                    atom.connect12.append(new_atom)
+                                    if new_atom.atomname not in [a.atomname for a in conf.atoms]:
+                                        completed_missing.add(new_atom)
+                            else:
+                                logging.error(f"   Atom \'{atom.atomname}\' in residue {resname} has only one connected atom, not able to place missing atoms")
+                                sys.exit(1)
+                        else:
+                            logging.error(f"   Atom \'{atom.atomname}\' in residue {resname} has no connected atom, not able to place missing atoms")
+                            sys.exit(1)
+            if completed_missing:
+                n_placed += len(completed_missing)
+                conf.atoms.extend(completed_missing)  # list can extend set elements
+                for added_atom in completed_missing:
+                    logging.info(f"   Placed atom {added_atom.atomname} in conformer {atom.parent_conf.confid}")
 
     return n_placed
--- bin/step2.py
+++ bin/step2.py
@@ -63,15 +63,7 @@ if __name__ == "__main__":
     # place missing heavy atoms
     mcce.assign_qr()
     mcce.make_connect12()
-    mcce.make_connect13()    
-    mcce.make_connect14()
-    # mcce.check_connect12()
-    # mcce.check_connect13()
-    # mcce.check_connect14()
-
-
-
-
+    logging.info("   Place missing heavy atoms ...")
     while True:
         if mcce.place_missing_heavy_atoms() == 0:   # place_missing_heavy_atoms() returns the number of atoms placed
             break
