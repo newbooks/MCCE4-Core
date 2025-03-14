1c9bd6edc66d5935a6b3b93b9a3e0096fb65f31a
newbooks <10424666+newbooks@users.noreply.github.com>
2025-03-14 09:48:21 -0400

    update

--- bin/mcce/_place_missing_heavy_atoms.py
+++ bin/mcce/_place_missing_heavy_atoms.py
@@ -2,10 +2,12 @@
 Place missing *heavy atoms* in the structure. All H atoms are ignored.
 """
 
+import sys
 import glob
 import logging
 from .pdbio import *
-
+from .geom import *
+from .constants import *
 
 def load_atom_templates(folder):
     """
@@ -33,7 +35,9 @@ def place_missing_heavy_atoms(self):    # self is a MCCE object
     templates = load_atom_templates(ideal_structures)
 
     for res in self.protein.residues:
-        for conf in res.conformers:
+        if len(res.conformers) < 2:  # only backbone
+            continue
+        for conf in res.conformers[1:]: # skip the first conformer, which is the backbone
             completed_missing = set()
             for atom in conf.atoms:
                 if atom.element != " H":
@@ -44,10 +48,33 @@ def place_missing_heavy_atoms(self):    # self is a MCCE object
                     missing = should_be_connected - actually_connected - {" ?  "} - completed_missing
                     if missing:
                         n_connected = len(actually_connected)
-                        if n_connected == 2:  # 2 known connected atoms, place the missing atom
+                        if n_connected >= 2:  # at least 2 known connected atoms, place the missing atom
                             resname = atom.resname
-                            
+                            c_atom_p = atom.xyz         # center atom coordinates
+                            c_atom_t = templates[resname][atom.atomname]  # center atom of the template coordinates
 
+                            # will get the two connected atoms
+                            check_atom = atom.connect12[0]  # pick one of the connected atoms
+                            if check_atom.atomname not in templates[resname]:
+                                logging.error(f"   Connected atom \'{check_atom.atomname}\' to \'{atom.atomname}\' not found in template for residue {resname}")
+                                sys.exit(1)
+                            a_atom_p = check_atom.xyz
+                            a_atom_t = templates[resname][check_atom.atomname]
 
+                            check_atom = atom.connect12[1]  # pick the other connected atom
+                            if check_atom.atomname not in templates[resname]:
+                                logging.error(f"   Connected atom \'{check_atom.atomname}\' to \'{atom.atomname}\' not found in template for residue {resname}")
+                                sys.exit(1)
+                            b_atom_p = check_atom.xyz
+                            b_atom_t = templates[resname][check_atom.atomname]
+
+                            # now we have all the coordinates, we can place the missing atom
+                            op = geom_3v_onto_3v(c_atom_t, a_atom_t, b_atom_t, c_atom_p, a_atom_p, b_atom_p)
+                            for atom_name in list(missing):
+                                new_atom = Atom()
+                                new_atom.atomname = atom_name
+                                new_atom.xyz = op.apply_to_vector(templates[resname][atom_name])  # apply the transformation to the template coordinates
+                                conf.atoms.append(new_atom)
+                                completed_missing.add(atom_name)
 
     return n_placed
