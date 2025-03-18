0c0d33f7025bb34ed16f2efed74d6b460e45d2e3
Junjun Mao <10424666+newbooks@users.noreply.github.com>
2025-03-18 13:18:26 -0400

    completed conftype propogation, working on H placement

--- bin/mcce/_conftype_comformers.py
+++ bin/mcce/_conftype_comformers.py
@@ -24,7 +24,7 @@ def propogate_conftypes(self):  # Here self is a MCCE object
     logging.info("   Propogate conftype conformers ...")
     # make a dictionary of conftype and its atom countes (CONNECT records)
     conftype_atom_count = {}
-    for key, value in self.tpl.items():
+    for key, _ in self.tpl.items():
         if key[0] == "CONNECT":
             conftype = key[2]
             if conftype not in conftype_atom_count:
@@ -36,4 +36,26 @@ def propogate_conftypes(self):  # Here self is a MCCE object
         if ("CONFLIST", res.resname) in self.tpl:
             new_confs = []
             conftype_names = self.tpl[("CONFLIST", res.resname)]
-            print(res.resname, conftype_names)
+            nondummy_conftype_names = set([c for c in conftype_names if c in conftype_atom_count and c[-2:] != "BK"])
+            # print(res.resname, conftype_names, nondummy_conftype_names)
+
+            # propogate from the existing non-BK conformers to the new conformers
+            if len(res.conformers) > 1:
+                for parent_conf in res.conformers[1:]:
+                    other_conftypes = nondummy_conftype_names -set([parent_conf.conftype])
+                    for conftype in other_conftypes:
+                        new_conf = parent_conf.clone()
+                        new_conf.conftype = conftype
+                        new_conf.history = conftype[-2:] + parent_conf.history[2:]
+                        new_confs.append(new_conf)
+            
+                # merge the new conformers to existing conformers
+                all_sidechain_confs = res.conformers[1:] + new_confs
+                ordered_confs = []
+                for conftype in conftype_names:
+                    ordered_confs.extend([c for c in all_sidechain_confs if c.conftype == conftype])  # sort by conftype in CONFLIST
+                res.conformers = [res.conformers[0]] + ordered_confs
+        else:
+            logging.warning(f"   {res.resname} has no CONFLIST record")
+
+    # add hydrogen atoms
