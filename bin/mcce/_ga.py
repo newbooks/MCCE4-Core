76ccbf1869a67010f9165a5665fe0b7e6ff30fd3
newbooks <10424666+newbooks@users.noreply.github.com>
2025-03-21 08:59:21 -0400

    done passing parameters to GA

--- bin/mcce/_ga.py
+++ bin/mcce/_ga.py
@@ -6,6 +6,55 @@ import logging
 import random
 from .constants import *
 
+class Pool:
+    """
+    Class for Pool in Genetic Algorithm
+    A pool has:
+    - size: number of individuals in the pool
+    - index_fixed: indices of fixed residues
+    - index_flipper: indices of flipper residues
+    - fixed_residues: list of fixed residues. This is shared by all individuals in the pool
+    - flipper_residues = individual: list of flipper residues. This list makes an individual
+    - population: list of individuals in the pool
+    """
+    def __init__(self, mcce, size):
+        self.size = size
+        self.index_fixed, self.index_flipper = self.divide_fixed_flipper(mcce)
+        self.fixed_residues = [mcce.protein.residues[i] for i in self.index_fixed]
+        self.population = []
+        # for i in range(size):
+        #     self.population.append(self.create_individual(mcce))
+    
+    def divide_fixed_flipper(self, mcce):
+        index_fixed = []
+        index_flipper = []
+        for i, residue in enumerate(mcce.protein.residues):
+            if len(residue.conformers) == 1:  # only backbone
+                index_fixed.append(i)
+            elif len(residue.conformers) == 2:  # one side chain
+                if residue.conformers[1].rotatables:
+                    index_flipper.append(i)
+                else:  # one side chain but no rotatable
+                    index_fixed.append(i)
+            else:  # more than one side chain
+                index_flipper.append(i)
+        return index_fixed, index_flipper
+
+    def create_individual(self, mcce):
+        """
+        Create an individual for the pool
+        """
+        individual = []
+        for i in self.index_flipper:
+            residue = mcce.protein.residues[i]
+            # randomly select a conformer from the flipper residues
+            selected_conformer = random.choice(residue.conformers)
+            selected_conformer.history = selected_conformer.history[:2] + "G" + selected_conformer.history[3:]  # mark as GA selected
+            individual.append(selected_conformer.clone())
+        # mix with fixed residues and create connect12 for each atom
+        return individual
+
+
 def ga_optimize(self):  # Here self is an instance of MCCE
     """
     Genetic Algorithm for MCCE
@@ -26,6 +75,6 @@ def ga_optimize(self):  # Here self is an instance of MCCE
     logging.info(f"      GA double_cross rate:{ga_double_cross}")
     logging.info(f"      GA mutation rate:{ga_mutation}")
 
-    
+    pool = Pool(mcce=self, size=ga_pool)
 
 
