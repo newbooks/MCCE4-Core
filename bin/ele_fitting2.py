402137bc26ddda26057d27115035ce214c195a15
Junjun Mao <10424666+newbooks@users.noreply.github.com>
2025-06-10 15:09:46 -0400

    update

--- bin/ele_fitting2.py
+++ bin/ele_fitting2.py
@@ -24,11 +24,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
-from xgboost import XGBRegressor
 import joblib
 import argparse
 import logging
